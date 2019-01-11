9a46456e1270c6ffcd7c62c5c5c9b383bbb3683a
Lawrence <mail@lawben.com>
2019-01-11 16:57:56 +0100

    Add init script

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Script to setup a new bakdata project from scratch.
+
+ZIP_NAME="bakdata-project-templates.tar.gz"
+DUMMY_DIR=".templates"
+
+# Get existing project templates
+echo -e "Downloading templates...\n"
+curl -Ls https://github.com/bakdata/bakdata-project-templates/archive/master.zip > $ZIP_NAME
+
+# Extract all templates for now
+mkdir -p $DUMMY_DIR && unzip -qq $ZIP_NAME -d $DUMMY_DIR
+BASE_DIR=$DUMMY_DIR/bakdata-project-templates-master
+
+echo "Select the project type to create"
+while [ -z "$SELECTED_PROJECT" ]; do
+  select SELECTED_PROJECT in $(ls $BASE_DIR);
+  do
+    break;
+  done
+done
+
+echo
+echo "Setting up $SELECTED_PROJECT..."
+
+cp -R "$BASE_DIR/$SELECTED_PROJECT/" .
+
+rm -rf $DUMMY_DIR
+rm $ZIP_NAME
+rm init.sh
