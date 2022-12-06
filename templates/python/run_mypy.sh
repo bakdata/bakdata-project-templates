79bce83918328a420b6e8a13fdc704b0f9852793
sujuka99 <vanko_yordanov@yahoo.com>
2022-12-06 09:54:13 +0100

    Removed unwanted files and packages. Replaced workflow templates with a call to the bakdata reusable poetry workflow.

--- templates/python/run_mypy.sh
+++ templates/python/run_mypy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 # from https://jaredkhan.com/blog/mypy-pre-commit
 
-set -o errexit
-
-# change directory to the project root directory.
-cd "$(dirname "$0")"
-
 mypy --pretty --package package-name --package tests --namespace-packages
